7132d8b5dfb147ffd2e552b3ad55bf90247e2dbc
Toffee <antoinette.zu@icloud.com>
2021-05-04 18:08:08 +0300

    laba 10 is done(dazhe ne veritsa)

--- lab10/src/components/SearchResult.tsx
+++ lab10/src/components/SearchResult.tsx
@@ -1,59 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { withRouter, RouteComponentProps } from "react-router-dom";
 import LoadingCard from "./LoadingCard";
 import NotFoundCard from "./NotFoundCard";
-import { IUser } from "./types";
 import UserCard from "./UserCard";
+import { useAppDispatch, useAppSelector } from "../Redux/hooks";
+import { getUserInfo } from "../Redux/UserSlice";
+import { FetchStatuses } from "../Redux/types";
 
 interface ISearchResultProps extends RouteComponentProps {
   username: string;
 }
 
-const initialState: IUser = {
-  login: "",
-  name: "",
-  location: "",
-  avatar_url: "",
-  html_url: "",
-  followers: 0,
-  following: 0,
-  blog: "",
-};
-
 const SearchResult: React.FC<ISearchResultProps> = ({ username }) => {
-  const [error, setError] = useState(false);
-  const [user, setUser] = useState<IUser>(initialState);
-  const [isLoading, setIsLoading] = useState(true);
+  const { error, status } = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (username.trim().length) {
-      fetch(`https://api.github.com/users/${username}`, {
-        method: "GET",
-        headers: {
-          Authorization: `Token ${process.env.REACT_APP_LAB10_KEY}`,
-        },
-      })
-        .then((res) => {
-          if (res.status === 404) {
-            setError(true);
-            return initialState;
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setUser(data);
-          setIsLoading(false);
-        });
+      dispatch(getUserInfo(username));
     }
-  }, [username]);
+  }, [dispatch, username]);
 
-  if (isLoading) {
+  if (status === FetchStatuses.idle) {
     return <LoadingCard />;
   }
   if (error) {
     return <NotFoundCard />;
   }
-  return <UserCard info={user} />;
+  return <UserCard />;
 };
 
 export default withRouter<ISearchResultProps, React.FC<ISearchResultProps>>(
--- lab10/src/components/UserCard.tsx
+++ lab10/src/components/UserCard.tsx
@@ -1,46 +1,45 @@
 import React from "react";
+import { useAppSelector } from "../Redux/hooks";
 import followers from "../icons/followers.svg";
 import location from "../icons/location.svg";
 import link from "../icons/link.svg";
 import BackButton from "./BackButton";
-import { IUser } from "./types";
 
-interface IUserCardProps {
-  info: IUser;
-}
-
-const UserCard: React.FC<IUserCardProps> = ({ info }) => (
-  <div className="user-card">
-    <img src={info.avatar_url} alt={info.login} className="avatar" />
-    <h1 className="login">{info.login}</h1>
-    <h2 className="name">{info.name}</h2>
-    <div className="followers">
-      <img src={followers} alt="followers" className="icon" />
-      {info.followers} followers &#9825; {info.following} following
+const UserCard: React.FC = () => {
+  const { user } = useAppSelector((state) => state.user);
+  return (
+    <div className="user-card">
+      <img src={user.avatar_url} alt={user.login} className="avatar" />
+      <h1 className="login">{user.login}</h1>
+      <h2 className="name">{user.name}</h2>
+      <div className="followers">
+        <img src={followers} alt="followers" className="icon" />
+        {user.followers} followers &#9825; {user.following} following
+      </div>
+      {user.location ? (
+        <div className="location">
+          <img alt="location" src={location} className="icon" />
+          {user.location}
+        </div>
+      ) : (
+        <></>
+      )}
+      {user.blog ? (
+        <div>
+          <img alt="link" src={link} className="icon" />
+          <a href={user.blog} rel="noreferrer" target="_blank">
+            {user.blog}
+          </a>
+        </div>
+      ) : (
+        <></>
+      )}
+      <a href={user.html_url} rel="noreferrer" target="_blank">
+        Открыть страницу пользователя на GitHub
+      </a>
+      <BackButton />
     </div>
-    {info.location ? (
-      <div className="location">
-        <img alt="location" src={location} className="icon" />
-        {info.location}
-      </div>
-    ) : (
-      <></>
-    )}
-    {info.blog ? (
-      <div>
-        <img alt="link" src={link} className="icon" />
-        <a href={info.blog} rel="noreferrer" target="_blank">
-          {info.blog}
-        </a>
-      </div>
-    ) : (
-      <></>
-    )}
-    <a href={info.html_url} rel="noreferrer" target="_blank">
-      Открыть страницу пользователя на GitHub
-    </a>
-    <BackButton />
-  </div>
-);
+  );
+};
 
 export default UserCard;
